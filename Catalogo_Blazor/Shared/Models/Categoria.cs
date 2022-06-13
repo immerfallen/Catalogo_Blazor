0a4751fec4aad2d72df610cb6d5dd7fb4d4921d0
immerfallen <marodemelo10@gmail.com>
2022-06-13 17:50:12 -0300

    Criadas classes produto e categoria

---
+++ Catalogo_Blazor/Shared/Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo_Blazor.Shared.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ Catalogo_Blazor/Shared/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo_Blazor.Shared.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public string ImagemUrl { get; set; }
+
+        public int CategoriId { get; set; }
+
+        public virtual Categoria Categoria { get; set; }
+    }
+}
