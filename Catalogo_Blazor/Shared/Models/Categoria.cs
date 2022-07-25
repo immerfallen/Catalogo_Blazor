aded773ff76bac5c27efd60f55ca9890e812ba64
immerfallen <marodemelo10@gmail.com>
2022-07-25 17:47:49 -0300

    Criado componente para inserir uma nova categoria

--- Catalogo_Blazor/Shared/Models/Categoria.cs
+++ Catalogo_Blazor/Shared/Models/Categoria.cs
@@ -10,10 +10,12 @@ namespace Catalogo_Blazor.Shared.Models
     public class Categoria
     {
         public int CategoriaId { get; set; }
-        
+
+        [Required(ErrorMessage ="O nome da categoria é obrigatório")]        
         [MaxLength(100)]
         public string? Nome { get; set; }
 
+        [Required(ErrorMessage = "A descrição da categoria é obrigatório")]
         [MaxLength(200)]
         public string? Descricao { get; set; }
 
